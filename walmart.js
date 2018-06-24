4e05bf40ad90c02e8e3b4100ff225ff7b34eb0c1
Dan Gross <danwgross@gmail.com>
2018-06-24 13:56:00 -0400

    working version complete

--- walmart.js
+++ walmart.js
@@ -11,28 +11,54 @@ $(document).on("click", "#submit-button", function (event) {
     url: productSearch + searchTerm,
     method: "GET"
   }).then(function(response) {
+    // TODO: make sure to remove console log when done testing
     console.log(response);
-    console.log(response.items[0].largeImage);
     for (i = 0; i < 10; i++) {
-      var img = $("<img>");
       var modalHeader = response.items[i].name;
       var modalBody = response.items[i].shortDescription;
       var modalPrice = response.items[i].salePrice;
-      var button = $("<button type='button' class='btn btn-primary modal-button' data-toggle='modal' data-target='#exampleModal'>Launch modal "+i+"</button>");
-      button.attr("data-body", modalBody);
-      button.attr("data-header", modalHeader);
-      button.attr("data-price", modalPrice);
-      img.addClass("image-size border border-dark")
-      img.attr("src", response.items[i].largeImage);
-      $("#test").append(img, button);
+      var cardDiv = $("<div class='card'>");
+      var cardImg = $("<img class='card-img-top' alt='Card image'>");
+      cardImg.attr("src", response.items[i].largeImage);
+      var cardBodyDiv = $("<div class='card-body'>");
+      var cardTitle = $("<h5 class='card-title'>"+modalHeader+"</h5>");
+      var cardFooterDiv = $("<div class='card-footer'>");
+      var cardButton = $("<button type='button' class='btn btn-primary modal-button' data-toggle='modal' data-target='#exampleModal'>More Info Modal</button>");
+      // FIXME: Need to get escape characters to not display quotes to user
+      cardButton.attr("data-body", modalBody);
+      cardButton.attr("data-header", modalHeader);
+      cardButton.attr("data-price", modalPrice);
+      cardBodyDiv.append(cardTitle);
+      cardFooterDiv.append(cardButton);
+      cardDiv.append(cardImg, cardBodyDiv, cardFooterDiv);
+      $("#test").append(cardDiv);
     };
   });
 });
 
 $(document).on("click", ".modal-button", function(event) {
   event.preventDefault();
+  // TODO: Make sure to remove this
   console.log($(this).attr("data-header"));
   $(".modal-title").text($(this).attr("data-header"));
   $(".modal-body").text($(this).attr("data-body"));
   $("#modal-price").text($(this).attr("data-price"));
-})
+});
+
+$(document).on("click", ".card", function() {
+  // TODO: Make sure to remove this
+  console.log(this);
+  $("#test").empty();
+  var yourChoice = $("<h1>Your Choice:");
+  var cardDiv = $("<div class='card'>");
+  var cardImgSRC = $(this).find("img").attr("src");
+  var cardImg = $("<img class='card-img-top' alt='Card image'>");
+  cardImg.attr("src", cardImgSRC);
+  cardImg.addClass("card-size");
+  var cardBodyDiv = $("<div class='card-body'>");
+  var cardTitleSRC = $(this).find("h5").text();
+  var cardTitle = $("<h5 class='card-title'>"+cardTitleSRC+"</h5>");
+  cardBodyDiv.append(cardTitle);
+  cardDiv.append(cardImg, cardBodyDiv);
+  $("#test").append(cardDiv);
+});
